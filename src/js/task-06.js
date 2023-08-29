f5d220bfad4a661c0457256c59da3ae24de01851
elias-291356 <ikucher.002@gmail.com>
2023-08-29 23:02:00 +0400

    update

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,12 +3,16 @@ const inputLength = +validationInput.dataset.length;
 console.log(validationInput);
 
 validationInput.addEventListener('blur', () => {
-  if (validationInput.value.length <= inputLength) {
+  if (validationInput.value.length === inputLength) {
     // validationInput.style.borderColor = 'teal';
-    validationInput.classList.toggle('valid');
+    validationInput.classList.add('valid');
+    validationInput.classList.remove('invalid');
   }
   else {
-    validationInput.classList.toggle('invalid');
+
+    validationInput.classList.remove('valid');
+    validationInput.classList.add('invalid');
     // validationInput.style.borderColor = 'tomato';
+
   }
 })
