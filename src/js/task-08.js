cd7abd21cf9ccff715454d5f3efff06bcdb256de
elias-291356 <ikucher.002@gmail.com>
2023-08-29 22:47:05 +0400

    update

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -2,7 +2,7 @@ const formRef = document.querySelector('.login-form');
 
 formRef.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (const inputLoginRef = formRef.elements.email.value; === '' || const inputPasswordRef = formRef.elements.password.value; === '') {
+  if (inputLoginRef = formRef.elements.email.value === '' || inputPasswordRef = formRef.elements.password.value === '') {
   alert('Всі поля повинні бути заповнені');
 } else {
   const credentials = { inputLoginRef, inputPasswordRef };
