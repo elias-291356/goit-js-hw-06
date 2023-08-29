b400aa9c964e015c36015bb622dad6d2adac63a2
elias-291356 <ikucher.002@gmail.com>
2023-08-29 23:10:51 +0400

    update 8task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,13 +1,19 @@
+
+
 const formRef = document.querySelector('.login-form');
+let inputLoginRef = formRef.elements.email.value;
+let inputPasswordRef = formRef.elements.password.value;
+
 
 formRef.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (inputLoginRef = formRef.elements.email.value === '' || inputPasswordRef = formRef.elements.password.value === '') {
-  alert('Всі поля повинні бути заповнені');
-} else {
-  const credentials = { inputLoginRef, inputPasswordRef };
-  formRef.reset();
-  console.log(credentials);
-}   
+
+  if (inputLoginRef.value === '' || inputPasswordRef.value === '') {
+    alert('Всі поля повинні бути заповнені');
+  } else {
+    const credentials = { inputLoginRef, inputPasswordRef };
+    formRef.reset();
+    console.log(credentials);
+  }
 });
 
